7a3b9c8613652f3bc4c834c558d4fc72e48377c7
rivermont <william.11bennett@gmail.com>
2017-10-16 12:46:16 -0400

    Added guessing game.

---
+++ guessingGame.cpp
@@ -0,0 +1,17 @@
+// A simple guessing game.
+
+#include <random>
+#include <iostream>
+
+int main() {
+    int i;
+
+    // Choose random number between 1 and 100
+    // While the guess is incorrect:
+        // Ask user to guess
+        // Tell user whether their guess was too high or too low
+    // If the guess is correct:
+        // Tell user their guess was correct
+
+    return 0 // Exit
+}
